98a94de3404a237b8382cdbf4207691056bb3323
Shekhawat2 <anandsingh215@yahoo.com>
2017-12-15 22:34:53 +0530

    init: fix for O
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- init/init_msm8916.cpp
+++ init/init_msm8916.cpp
@@ -36,8 +36,11 @@
 
 #include <android-base/properties.h>
 #include <android-base/strings.h>
+#include <android-base/properties.h>
+#include <android-base/file.h>
+#include <android-base/logging.h>
 
-#include "log.h"
+#include "vendor_init.h"
 #include "property_service.h"
 #include "util.h"
 #include "vendor_init.h"
@@ -46,6 +49,9 @@
 
 using android::base::GetProperty;
 using android::base::Trim;
+using android::base::ReadFileToString;
+using android::base::GetProperty;
+using android::init::property_set;
 
 __attribute__ ((weak))
 void init_target_properties()
@@ -58,10 +64,10 @@ static void init_alarm_boot_properties()
     char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
     std::string boot_reason;
     std::string power_off_alarm;
-    std::string tmp = GetProperty("ro.boot.alarmboot");
+    std::string tmp = GetProperty("ro.boot.alarmboot","");
 
-    if (read_file(boot_reason_file, &boot_reason)
-            && read_file(power_off_alarm_file, &power_off_alarm)) {
+    if (ReadFileToString(boot_reason_file, &boot_reason)
+            && ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
         /*
          * Setup ro.alarm_boot value to true when it is RTC triggered boot up
          * For existing PMIC chips, the following mapping applies
