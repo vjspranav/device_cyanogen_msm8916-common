ee13c5340c311696908e8442abedefb4e470bd68
jrior001 <jriordan001@gmail.com>
2018-02-14 20:56:52 -0500

    msm8916: move permisions to vendor
    Change-Id: I0ab10488b17ce7ab57c22f466b4f1a03e29ce363

--- product/usb.mk
+++ product/usb.mk
@@ -4,8 +4,12 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+<<<<<<< HEAD
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
 
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
+=======
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/vendor/etc/permissions/android.hardware.usb.accessory.xml
+>>>>>>> 9bf90ef... msm8916: move permisions to vendor
