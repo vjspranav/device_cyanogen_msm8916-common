9f08966089cd98595b45d21149674f96f6db06f8
Michael Bestas <mkbestas@lineageos.org>
2018-03-16 02:11:53 +0200

    msm8916-common: Binderize them all
    Change-Id: I4fe70aca641e961d28497f8c75862ee85fd33941

---
+++ product/gps.mk
@@ -0,0 +1,9 @@
+# GNSS HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
+
+# GPS Shim
+PRODUCT_PACKAGES += \
+    libshims_get_process_name \
+    libshims_flp
