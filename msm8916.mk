8db02a50ef3f1f38878bcb7a04e653621cd1a250
ZeeRooo <chavesjuan400@gmail.com>
2019-01-15 18:40:24 -0300

    msm8916-common: Use XML audio policy configuration file from LA.BR.1.2.9.1-02310-8x16.0
    Inspired by: https://android.googlesource.com/device/lge/bullhead/+/b1d6440e7%5E%21/#F2
    
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -46,7 +46,13 @@ PRODUCT_PACKAGES += \
 #   android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/vendor/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/vendor/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/vendor/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/vendor/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/vendor/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/vendor/etc/usb_audio_policy_configuration.xml
 
 # GPS Shim
 PRODUCT_PACKAGES += \
